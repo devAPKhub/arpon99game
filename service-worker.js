3ad734b5dfd6784c14ebd0e616495ca8cbd30c1b
Your Name <your.email@example.com>
2025-12-15 00:19:38 +0600

    Initial commit of project-2

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "color-match-game-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
